abc90808e43065f4617910a410e7e6e33da9179b
Olivier <olivier.ledc@gmail.com>
2023-06-06 14:11:23 +0200

    Components setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuizPageComponent } from './quiz-page/quiz-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'quiz', component: QuizPageComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuizPageComponent } from './quiz-page/quiz-page.component';
+import { MyQuizPageComponent } from './my-quiz-page/my-quiz-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizPageComponent,
+    MyQuizPageComponent,
   ],
   imports: [
     BrowserModule,
