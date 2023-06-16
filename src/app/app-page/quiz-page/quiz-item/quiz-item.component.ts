9d2f7703b92e57c1dde2ba45b629769b92aef9e4
Olivier Leduc <olivier.ledc@gmail.com>
2023-06-16 21:45:12 +0200

    cfgnhswdgfhwdfbwdfbfwdb

--- src/app/app-page/quiz-page/quiz-item/quiz-item.component.ts
+++ src/app/app-page/quiz-page/quiz-item/quiz-item.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
+import { Category } from 'src/classes/Category';
 import { Quiz } from 'src/classes/Quiz';
 import { User } from 'src/classes/User';
+import { CategoriesService } from 'src/services/category.service';
 import { UsersService } from 'src/services/users.service';
 
 @Component({
@@ -11,18 +13,28 @@ import { UsersService } from 'src/services/users.service';
 export class QuizItemComponent {
   @Input() quiz: Quiz;
   user: User;
+  category: Category;
 
   constructor(
-    private userService: UsersService){
+    private userService: UsersService,
+    private categoryService: CategoriesService)
+  {
     this.quiz = new Quiz();
     this.user = new User();
+    this.category = new Category();
   }
 
   ngOnInit():void{
     this.getUser();
+    this.getCategory();
+    
   }
 
   private getUser(): void{
     this.userService.getById(this.quiz.user_id).subscribe(user => this.user = user);
+  }
+
+  private getCategory(): void{
+    this.categoryService.getById(this.quiz.category_id).subscribe(category => this.category = category);
   }
 }
