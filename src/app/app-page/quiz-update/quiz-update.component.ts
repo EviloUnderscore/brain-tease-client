438abb28618641767710f184c3100ae17818f5b7
Olivier Leduc <olivier.ledc@gmail.com>
2023-06-18 14:00:50 +0200

    n

--- src/app/app-page/quiz-update/quiz-update.component.ts
+++ src/app/app-page/quiz-update/quiz-update.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { Categories } from 'src/classes/Categories';
 import { Quiz } from 'src/classes/Quiz';
 import { CategoriesService } from 'src/services/category.service';
+import { QuizzesService } from 'src/services/quizzes.service';
 
 @Component({
   selector: 'quiz-update',
@@ -15,7 +16,8 @@ export class QuizUpdateComponent {
 
   constructor(
     private router: Router,
-    private categoriesService: CategoriesService){
+    private categoriesService: CategoriesService,
+    private quizzesService: QuizzesService){
     this.quiz = new Quiz();
     this.categories = new Categories();
   }
@@ -30,7 +32,17 @@ export class QuizUpdateComponent {
   }
 
   submitForm(): void{
-    console.log(this.quiz);
-    
+    if(this.isFormValid()){
+      this.quizzesService.createQuiz(this.quiz.name, this.quiz.description, this.quiz.category_id).subscribe(() => {
+        this.router.navigate(['/my-quizzes']);
+      })
+    }
+  }
+
+  isFormValid(): boolean {
+    if (!this.quiz.name || !this.quiz.description || !this.quiz.category_id) {
+      return false;
+    }
+    return true;
   }
 }
